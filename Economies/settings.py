835f3588b238c4ddb28d7161e36db625bb26f5d2
Gabriel Ghisleni <gabriele.ghisleni01@gmail.com>
2021-11-08 11:32:05 +0100

    heroku

--- Economies/settings.py
+++ Economies/settings.py
@@ -11,9 +11,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+from pathlib import Path
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
