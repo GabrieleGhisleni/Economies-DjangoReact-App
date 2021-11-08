8a591c6e205ee4e50a6456cbd72682e6abbe3ad7
Gabriel Ghisleni <gabriele.ghisleni01@gmail.com>
2021-11-08 17:48:27 +0100

    .

--- users/signal.py
+++ users/signal.py
@@ -1,7 +1,6 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from .models import MyUser
 from records.models import Members
 from records.models import UserCategory
 from django.contrib.auth.models import User
@@ -9,10 +8,17 @@ from django.contrib.auth.models import User
 @receiver(post_save, sender=User)
 def create_member(sender, instance, created, **kwargs):
     if created:
-        print('Receiving Signal')
         defaultCategory = ['Auto', 'Sport','Alimentari','Ristorante','Svago','Viaggi']
-        Members.objects.create(user=instance, member_name='Comune')
-        Members.objects.create(user=instance, member_name=instance.username)
-        for cat in defaultCategory: UserCategory.objects.create(category_name=cat, user=instance)
+        try:
+            Members.objects.create(user=instance, member_name='Comune')
+            Members.objects.create(user=instance, member_name=instance.username)
+            for cat in defaultCategory: 
+                UserCategory.objects.create(category_name=cat, user=instance)
+        except:
+            Members(user=instance, member_name='Comune').save()
+            Members(user=instance, member_name=instance.username).save()
+            for cat in defaultCategory: 
+                UserCategory(category_name=cat, user=instance).save()
         post_save.connect(create_member, sender=User)
+
 
