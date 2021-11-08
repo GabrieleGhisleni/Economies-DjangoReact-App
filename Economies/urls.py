d60110cd68c6da98e85ecb4ac56efb53c4f44e98
Gabriel Ghisleni <gabriele.ghisleni01@gmail.com>
2021-11-08 11:53:33 +0100

    .

--- Economies/urls.py
+++ Economies/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 from users.auth.viewset import LoginViewSet, RegistrationViewSet, RefreshViewSet
-
+from django.views.generic.base import TemplateView
 from rest_framework import routers
 from records import views
 
